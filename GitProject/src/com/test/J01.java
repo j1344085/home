72eb6864d129282cb98373ba7aa6df5dfabbce78
201344085_LyuSeongMin <j1344085@naver.com>
2016-09-30 19:29:52 +0900

    create project

---
+++ GitProject/src/com/test/J01.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class J01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub  // ¡÷ºÆ Ctrl+/
+		Scanner s = new Scanner(System.in); // Ctrl+shift+o
+		System.out.println("Hello");
+	}
+
+}
